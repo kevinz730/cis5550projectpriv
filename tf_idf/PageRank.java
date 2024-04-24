0e3e5b55007b5e5e6b2cc9fb1b8f9dd4423c1d53
Manvikaul <manvikaul2000@gmail.com>
2024-04-24 01:43:40 -0400

    pageRank updated

--- tf_idf/PageRank.java
+++ tf_idf/PageRank.java
@@ -2,10 +2,13 @@ package cis5550.jobs;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import cis5550.flame.FlameContext;
 import cis5550.flame.FlamePair;
@@ -13,9 +16,115 @@ import cis5550.flame.FlamePairRDD;
 import cis5550.flame.FlameRDD;
 import cis5550.kvs.KVSClient;
 import cis5550.tools.Hasher;
+import cis5550.tools.URLParser;
 
 public class PageRank {
 	
+	public static List<String> urlNormalizer(List<String> urls, String base) throws MalformedURLException {
+		
+		ArrayList<String> normalizedUrls = new ArrayList<String>();
+		try {
+		
+		String baseUrl = base;
+		for (String url : urls) {
+			String[] urlParts = URLParser.parseURL(url);
+			String protocol = urlParts[0];
+			String hostName = urlParts[1];
+			String port = urlParts[2];
+			String rest = urlParts[3];
+			if (rest.indexOf('#') > -1) {
+				rest = rest.substring(0, rest.indexOf('#'));
+				if (protocol == null && hostName == null && port == null && rest.equals("")) {
+					normalizedUrls.add(baseUrl);
+					continue;
+				}
+			}
+			if (rest.lastIndexOf('.') > -1) {
+				String end = rest.substring(rest.lastIndexOf('.'));
+				if (end.equals(".jpg") || end.equals(".jpeg") || end.equals(".gif") || end.equals(".png") || end.equals(".txt")) {
+					continue;
+				}
+			}
+			if (protocol == null && hostName == null && port == null) {
+//				Either relative or absolute path without hostname
+//				Cuts off part in base URL after last /
+				String modifiedBaseUrl = baseUrl.substring(0, baseUrl.lastIndexOf('/'));
+				if (rest.charAt(0) == '/') {
+//					absolute path
+					String[] baseUrlParts = URLParser.parseURL(baseUrl);
+					modifiedBaseUrl = baseUrlParts[0] + "://" + baseUrlParts[1] + ":" + baseUrlParts[2];
+					normalizedUrls.add(modifiedBaseUrl+rest);
+					continue;
+				} else {
+					while (rest.substring(0, 3).equals("../")) {
+						modifiedBaseUrl = modifiedBaseUrl.substring(0, modifiedBaseUrl.lastIndexOf('/'));
+						rest = rest.substring(3);
+					}
+					normalizedUrls.add(modifiedBaseUrl+"/"+rest);
+					continue;
+				}
+			}
+			else if (port == null) {
+				String modifiedBaseUrl = "";
+				if (protocol.equals("http")) {
+					modifiedBaseUrl = protocol + "://" + hostName + ":" + "80";
+				} else if (protocol.equals("https")){
+					modifiedBaseUrl = protocol + "://" + hostName + ":" + "443";
+				} else {
+					continue;
+				}
+				normalizedUrls.add(modifiedBaseUrl+rest);
+				continue;
+			}
+			normalizedUrls.add(url);
+		}
+		}catch(Exception e){
+			normalizedUrls.add("");
+		}
+		return normalizedUrls;
+
+		
+		 
+	}
+	
+	public static List<String> urlExtract(byte[] data) {
+		ArrayList<String> urls = new ArrayList<String>();
+		String htmlText = new String(data);
+		Pattern pattern = Pattern.compile("<([^/][^>]*)>");
+		Matcher matcher = pattern.matcher(htmlText);
+		while (matcher.find()) {
+			String tag = matcher.group(1);
+			String[] tagParts = tag.split("\\s+");
+			String tagType = tagParts[0];
+			if (tagType.toLowerCase().equals("a")) {
+				for (String s: tagParts) {
+					if(s.contains("href=")) {
+						int idx = s.indexOf('=');
+						String url = s.substring(idx + 1);
+						String finalUrl = url.substring(1, url.length() - 1);
+						urls.add(finalUrl);
+					}
+				}
+			}
+		}
+		return urls;
+	}
+	
+	public static List<String> extractUrls(String pageData) {
+		//System.out.println("page data " + pageData);
+        List<String> urls = new ArrayList<>();
+        //TODO: confirm matcher
+        Pattern pattern = Pattern.compile("<a\\s+(?:[^>]*?\\s+)?(?i)href=\"([^\"]*)\"", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(pageData);
+
+        while (matcher.find()) {
+            String url = matcher.group(1).trim();
+            System.out.println("matched url " + url);
+            urls.add(url);
+        }
+
+        return urls;
+    }
 	
 	
 	public static void run(FlameContext context, String[] stringArr) {
@@ -29,32 +138,40 @@ public class PageRank {
 			    FlameRDD stateTable = context.fromTable("pt-crawl", row -> {
 				String u = row.get("url");
 				String p = row.get("page");
+				System.out.println("url " + u + " page "+p);
 				List<String> urls = new ArrayList<>();
-				urls = Crawler.extractUrls(p);
-				
+				if(p!=null) {
+					byte[] b = p.getBytes();
+					//urls = urlExtract(b);
+					urls = extractUrls(p);
+				}
 				Hasher hasher = new Hasher();
 				
 				List<String> normalizedUrls = new ArrayList<>();
 				try {
-					normalizedUrls = Crawler.normalizeUrl(urls, u);
+					normalizedUrls = urlNormalizer(urls, u);
 				} catch (MalformedURLException e) {
 					e.printStackTrace();
 				}
 				
 				LinkedHashSet<String> lset= new LinkedHashSet<>(); 
+				LinkedHashSet<String> unhashed= new LinkedHashSet<>(); 
 				for(int i = 0; i<normalizedUrls.size(); i++)
 				{
 					
 					if(!normalizedUrls.get(i).equals("")) {
 						String hashed = hasher.hash(normalizedUrls.get(i));
 	            		lset.add(hashed);
+	            		//lset.add(normalizedUrls.get(i));
 	            	}
 				}
 				
 				String l = String.join(",", lset);
+				String test = String.join(",", unhashed);
 	            
 				String hashedu = hasher.hash(u);
 	            return hashedu+",1.0,1.0,"+l; 
+				
 	        });
 			    
 			FlamePairRDD flamePair = stateTable.mapToPair(input -> {
@@ -162,7 +279,8 @@ public class PageRank {
 		
 					double maxDiff = Double.parseDouble(maxRankChange);
 
-					double convThreshold = Double.parseDouble(stringArr[0]);
+					//double convThreshold = Double.parseDouble(stringArr[0]);
+					double convThreshold = 0.01;
 					if(maxDiff < convThreshold) break;
 			}
 			
