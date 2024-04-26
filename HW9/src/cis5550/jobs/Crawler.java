db9f88116ec2d204d2bbbc93597e25b345d88ec1
Spencer Mateega <spencer.mateega@gmail.com>
2024-04-25 23:53:00 -0400

    update urlFilter

--- HW9/src/cis5550/jobs/Crawler.java
+++ HW9/src/cis5550/jobs/Crawler.java
@@ -110,32 +110,36 @@ public class Crawler {
 	}
 	
 	public static List<String> urlFilter(List<String> urls) {
-		List<String> filtered = new ArrayList<String>();
-		for (String url: urls) {
-			if (url.contains("creativecommons.org") || url.contains("robtex.com")) {
-				continue;
-			}
-			if (url.contains("wiki")) {
-				int periodIndex = url.indexOf('.');
-		        if (periodIndex != -1 && periodIndex >= 2) {
-		            String lang = url.substring(periodIndex - 2, periodIndex);
-		            if (!lang.equals("en")) {
-		            	continue;
-		            }
-		        }
-			}
-			int lastPeriodIndex = url.lastIndexOf('.');
-	        if (lastPeriodIndex != -1 && lastPeriodIndex < url.length() - 3) {
-	            String tld = url.substring(lastPeriodIndex + 1, lastPeriodIndex + 4);
-	            if (!tld.equals("com") && !tld.equals("org") && !tld.equals("net") && !tld.equals("edu") && 
-	            		!tld.equals("int") && !tld.equals("gov") && !tld.equals("mil")) {
-	            	continue;
-	            }
-	        }
-			filtered.add(url);
-		}
-		return filtered;
-	}
+    List<String> filtered = new ArrayList<String>();
+      for (String url: urls) {
+        if (url.contains("creativecommons.org") || url.contains("robtex.com") || url.contains("%")
+          || url.contains("aljazeeramubasher") || url.contains("poemhunter")) {
+          continue;
+        }
+        if (url.contains("wiki")) {
+          if (!url.contains("wikipedia") && !url.contains("wikinews")) {
+            continue;
+          }
+          int periodIndex = url.indexOf('.');
+          if (periodIndex != -1 && periodIndex >= 2) {
+            String lang = url.substring(periodIndex - 2, periodIndex);
+            if (!lang.equals("en")) {
+              continue;
+            }
+          }
+        }
+        int lastPeriodIndex = url.lastIndexOf('.');
+        if (lastPeriodIndex != -1 && lastPeriodIndex < url.length() - 3) {
+          String tld = url.substring(lastPeriodIndex + 1, lastPeriodIndex + 4);
+          if (!tld.equals("com") && !tld.equals("org") && !tld.equals("net") && !tld.equals("edu") && 
+            !tld.equals("int") && !tld.equals("gov") && !tld.equals("mil")) {
+            continue;
+          }
+        }
+      filtered.add(url);
+    }
+    return filtered;
+  }
 	
 	public static String urlSeedNormalize(String base) {
 		String url = base;
