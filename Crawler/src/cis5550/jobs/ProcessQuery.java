1c11d3cd47c5539c9cbdba5315188d9437297084
Manvikaul <manvikaul2000@gmail.com>
2024-04-26 15:54:07 -0400

    query processing

--- Crawler/src/cis5550/jobs/ProcessQuery.java
+++ Crawler/src/cis5550/jobs/ProcessQuery.java
@@ -14,18 +14,33 @@ public class ProcessQuery {
 	
 	public static void run(FlameContext context, String[] args) throws Exception{
 		
-		ConcurrentHashMap<String, Double> queryTF = new ConcurrentHashMap<>();
+		ConcurrentHashMap<String, Double> queryTF_map = new ConcurrentHashMap<>();
+		ConcurrentHashMap<String, Double> queryTF_IDF_map = new ConcurrentHashMap<>();
 		String query = args[0];
 		System.out.println("query: " + query);
 		
 		String[] queryParts = query.split(" ");
 		for(String s: queryParts) {
-			double freq = queryTF.getOrDefault(s, 0.0);
-			queryTF.put(s, freq+1);
+			double freq = queryTF_map.getOrDefault(s, 0.0);
+			queryTF_map.put(s, freq+1);
 		}
-		for(Map.Entry<String, Double> e: queryTF.entrySet()) {
-			queryTF.put(e.getKey(), e.getValue()*1.0/query.length());
+		
+		for (Map.Entry<String, Double> entry : queryTF_map.entrySet()) {
+			System.out.println("***** query freq*****");
+            System.out.println("Word: " + entry.getKey() + ", Frequency: " + entry.getValue());
+        }
+		
+		for(Map.Entry<String, Double> e: queryTF_map.entrySet()) {
+			System.out.println("map val " + e.getValue() + " as double " + e.getValue()*1.0 + " len " + query.length());
+			queryTF_map.put(e.getKey(), (e.getValue()*1.0)/queryParts.length);
 		}
+		
+		for (Map.Entry<String, Double> entry : queryTF_map.entrySet()) {
+			System.out.println("***** query tf*****");
+            System.out.println("Word: " + entry.getKey() + ", Frequency: " + entry.getValue());
+        }
+		
+		
 		
 		KVSClient k = context.getKVS();
 		int numDocs = k.count("pt-crawl");
@@ -46,23 +61,35 @@ public class ProcessQuery {
 				Set<String> cols = r.columns();
 				int numDocsWithTerm = cols.size();
 				int j = 0;
+				
+				queryIDF = Math.log((numDocs)*1.0/numDocsWithTerm);
+			    System.out.println("queryIDF " + queryIDF);
+			    System.out.println("queryTF.get(queryParts[i]) " + queryTF_map.get(queryParts[i]));
+		    	queryTF_IDF = queryTF_map.get(queryParts[i]) * queryIDF;
+		    	queryTF_IDF_map.put(queryParts[i], queryTF_IDF);
+		    	System.out.println("queryTF_IDF " + queryTF_IDF);
+		    	
 				for (String col : cols) {
 				    System.out.println("j " + j + " col " + col);
 				    j++; 
 				    
-				    queryIDF = (numDocs)*1.0/numDocsWithTerm;
-				    System.out.println("queryIDF " + queryIDF);
-			    	queryTF_IDF = queryTF.get(queryParts[i]) * queryIDF;
-			    	System.out.println("queryTF_IDF " + queryTF_IDF);
+//				    queryIDF = Math.log((numDocs)*1.0/numDocsWithTerm);
+//				    System.out.println("queryIDF " + queryIDF);
+//				    System.out.println("queryTF.get(queryParts[i]) " + queryTF_map.get(queryParts[i]));
+//			    	queryTF_IDF = queryTF_map.get(queryParts[i]) * queryIDF;
+//			    	queryTF_IDF_map.put(queryParts[i], queryTF_IDF);
+//			    	System.out.println("queryTF_IDF " + queryTF_IDF);
 			    	
 			    	String tf_idf = r.get(col);
 			    	System.out.println("tf_idf " + tf_idf);
 			    	double docTF_IDF = Double.parseDouble(tf_idf);
 			    	System.out.println("docTF_IDF " + docTF_IDF);
 			    	
-				    if(!computedVals.contains(col)) {
-				    	System.out.println("!computedVals.contains(col) new val added " + tf_idf);
+			    	
+				    if(!computedVals.containsKey(col)) {
 				    	computedVals.put(col, String.valueOf(docTF_IDF * queryTF_IDF));
+				    	System.out.println("!computedVals.contains(col) new val added " + String.valueOf(docTF_IDF * queryTF_IDF));
+
 				    }
 				    else  {
 				    	String prevVal = computedVals.get(col);
@@ -77,44 +104,40 @@ public class ProcessQuery {
 				    System.out.println("docWeight " + String.valueOf(docWeight));
 				}
 				
-				double queryWeight = 0.0;
-				for(Map.Entry<String, Double> e: queryTF.entrySet()) {
-					queryWeight += Math.pow(queryTF.get(e.getKey())*queryIDF, 2);
-					System.out.println("queryWeight in for loop " + String.valueOf(queryWeight));
-				}
-				queryWeight = Math.sqrt(queryWeight);
-				System.out.println("queryWeight " + String.valueOf(queryWeight));
 				
-		    	//Doc weight
-		    	docWeight = Math.sqrt(docWeight);
-		    	System.out.println("docWeight after sqrt " + String.valueOf(docWeight));
-
-		    	for(Map.Entry<String, String> e:computedVals.entrySet()) {
-		    		String col = e.getKey();
-		    		System.out.println("col " + col);
-		    		double cosSim = Double.parseDouble(computedVals.get(col)) / (docWeight * queryWeight);
-		    		System.out.println("cosSim " + String.valueOf(cosSim));
-		    		
-		    		String hashedVal = Hasher.hash(col);
-				    Row pageRank = k.getRow("pt-pageranks", hashedVal);
-				    String data = pageRank.get("rank");
-				    System.out.println("data " + data);
-				    double dataDouble = Double.parseDouble(data);
-				    System.out.println("dataDouble " + String.valueOf(dataDouble));
-			    	double newVal = dataDouble + cosSim;
-			    	System.out.println("newVal " + String.valueOf(newVal));
-			    	computedVals.put(col, String.valueOf(newVal));
-		    	}
 		    	
-		    	//PageRank Calculation
 		    	
 			}
 		}
 		
-		for(Map.Entry<String, Double> e: queryTF.entrySet())
-		{
-			
+		double queryWeight = 0.0;
+		for(Map.Entry<String, Double> e: queryTF_IDF_map.entrySet()) {
+			queryWeight += Math.pow(queryTF_IDF_map.get(e.getKey()), 2);
+			System.out.println("queryWeight in for loop " + String.valueOf(queryWeight));
 		}
+		queryWeight = Math.sqrt(queryWeight);
+		System.out.println("queryWeight " + String.valueOf(queryWeight));
+		
+    	//Doc weight
+    	docWeight = Math.sqrt(docWeight);
+    	System.out.println("docWeight after sqrt " + String.valueOf(docWeight));
+
+    	for(Map.Entry<String, String> e:computedVals.entrySet()) {
+    		String col = e.getKey();
+    		System.out.println("col " + col);
+    		double cosSim = Double.parseDouble(computedVals.get(col)) / (docWeight * queryWeight);
+    		System.out.println("cosSim " + String.valueOf(cosSim));
+    		
+    		String hashedVal = Hasher.hash(col);
+		    Row pageRank = k.getRow("pt-pageranks", hashedVal);
+		    String data = pageRank.get("rank");
+		    System.out.println("data " + data);
+		    double dataDouble = Double.parseDouble(data);
+		    System.out.println("dataDouble " + String.valueOf(dataDouble));
+	    	double newVal = dataDouble + cosSim;
+	    	System.out.println("newVal " + String.valueOf(newVal));
+	    	computedVals.put(col, String.valueOf(newVal));
+    	}
 		
 		 computedVals.entrySet()
          .stream()
